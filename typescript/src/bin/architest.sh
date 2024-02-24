542637848877584666bbd0df00330d5803c294f9
0x5afe <archikoder@gmail.com>
2024-02-24 12:43:30 +0100

    fixed shell script

--- typescript/src/bin/architest.sh
+++ typescript/src/bin/architest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+executable_path=$(dirname $(readlink -f "$0"))
+
 if [[ "$*" == *"--watch"* ]]; then
-    /usr/bin/env tsx watch --tsconfig $(pwd)/tsconfig.architest.json $(pwd)/dist/bin/architest.js "${@/ --watch/}"
+    /usr/bin/env tsx watch --tsconfig $executable_path/../../tsconfig.architest.json $executable_path/architest.js "${@/ --watch/}"
 else
-    /usr/bin/env tsx --tsconfig $(pwd)/tsconfig.architest.json $(pwd)/dist/bin/architest.js "$@"
+    /usr/bin/env tsx --tsconfig $executable_path/../../tsconfig.architest.json $executable_path/architest.js "$@"
 fi
